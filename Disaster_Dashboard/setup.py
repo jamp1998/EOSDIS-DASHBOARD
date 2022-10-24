20845c8391a45b66ad77b4b0ddf3ed9ee77c63c4
jamp1998 <joelamp98@gmail.com>
2022-10-24 05:40:50 -0300

    Commit jupyter noteboo

---
+++ Disaster_Dashboard/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Create a dashboard toreport stadistict from data colected by EOSDIS missions.',
+    author='Joel Martinez',
+    license='',
+)
